36fde3c28ca15cb608cc1931c862e727f9e1b5d5
Ayush Jain <ayushjain.dce@gmail.com>
2016-06-22 02:38:31 +0530

    added binary tree problems

--- CrackCodingInterview/Trees/src/BinaryTree.java
+++ CrackCodingInterview/Trees/src/BinaryTree.java
@@ -1,6 +1,4 @@
-/**
- * Created by ayush on 16/6/16.
- */
+
 import  java.util.*;
 
 public class BinaryTree {
@@ -44,6 +42,40 @@ public class BinaryTree {
         return root;
     }
 
+    // Create sample tree 2
+//                             1
+//                           /   \
+//                          2     3
+//                         /     / \
+//                        4     5   6
+//                       /  \
+//                      7    8
+//                          / \
+//                         9   10
+
+    public static TNode createSampleTree2(){
+        TNode root = new TNode(1);
+        TNode a = new TNode(2);
+        TNode b = new TNode(3);
+        TNode c = new TNode(4);
+        TNode d = new TNode(5);
+        TNode e = new TNode(6);
+        TNode f = new TNode(7);
+        TNode g = new TNode(8);
+        TNode h = new TNode(9);
+        TNode i = new TNode(10);
+        root.setLeft(a);
+        root.setRight(b);
+        a.setLeft(c);
+        b.setLeft(d);
+        b.setRight(e);
+        c.setLeft(f);
+        c.setRight(g);
+        g.setLeft(h);
+        g.setRight(i);
+        return root;
+    }
+
     // Recursive InOrder Traversal
     public static void inOrderTraversal(TNode root){
         if(root != null){
@@ -55,6 +87,9 @@ public class BinaryTree {
 
     // Level Order Traversal
     public static void levelOrderTraversal(TNode root){
+        if(root == null){
+            return;
+        }
         LinkedList<TNode> queue = new LinkedList<TNode>();
         queue.add(root);
         while (!queue.isEmpty()){
@@ -66,6 +101,29 @@ public class BinaryTree {
             if(node.getRight() != null){
                 queue.add(node.getRight());
             }
+        }
+    }
+    // Level Order Traversal Line by Line
+    public static void levelOrderTraversalLineByLine(TNode root){
+        if(root == null){
+            return;
+        }
+        LinkedList<TNode> queue = new LinkedList<TNode>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int count = queue.size();
+            while(count > 0){
+                TNode node = queue.remove();
+                System.out.print(node.getData() + " ");
+                if(node.getLeft() != null){
+                    queue.add(node.getLeft());
+                }
+                if(node.getRight() != null){
+                    queue.add(node.getRight());
+                }
+                count--;
+            }
+            System.out.println();
         }
     }
 
@@ -114,6 +172,87 @@ public class BinaryTree {
         return 1 + Math.max(leftHeight,rightHeight);
     }
 
+    // Height of Binary Tree using Level Order Traversal
+    public static int heightOfTreeUsingLevelOrder(TNode root){
+        if(root == null){
+            return 0;
+        }
+        LinkedList<TNode> queue = new LinkedList<TNode>();
+        queue.add(root);
+        int height = 0;
+        while(!queue.isEmpty()){
+            int count = queue.size();
+            height++;
+            while(count > 0){
+                TNode node = queue.remove();
+                if(node.getLeft() != null){
+                    queue.add(node.getLeft());
+                }
+                if(node.getRight() != null){
+                    queue.add(node.getRight());
+                }
+                count--;
+            }
+        }
+        return height;
+    }
+
+    // Minimum depth of tree (Number of nodes in the shortest path from root to any leaf)
+    public static int minDepth(TNode root){
+
+        // This case will be hit only if the function is called initially with null root
+        if(root == null){
+            return 0;
+        }
+        if(root.getLeft() == null && root.getRight() == null){
+            return 1;
+        }
+        if(root.getLeft() == null){
+            return 1 + minDepth(root.getRight());
+        }
+        if(root.getRight() == null){
+            return 1 + minDepth(root.getLeft());
+        }
+        return 1 + Math.min(minDepth(root.getLeft()),minDepth(root.getRight()));
+    }
+
+    // Minimum depth of tree using Level Order Traversal
+    public static int minDepthUsingLevelOrderTraversal(TNode root){
+        if(root == null){
+            return 0;
+        }
+        int minDepth = 0;
+        LinkedList<TNode> queue = new LinkedList<TNode>();
+        queue.add(root);
+        while(true){
+            int count = queue.size();
+            minDepth++;
+            while(count > 0){
+                TNode node = queue.remove();
+                if(node.getLeft() == null && node.getRight() == null){
+                    return minDepth;
+                }
+                if(node.getLeft() != null){
+                    queue.add(node.getLeft());
+                }
+                if(node.getRight() != null){
+                    queue.add(node.getRight());
+                }
+                count--;
+            }
+        }
+    }
+
+    // Size of tree (Number of Nodes in the tree)
+    public static int sizeOfTree(TNode root){
+        if(root == null){
+            return 0;
+        }
+        int leftSize = sizeOfTree(root.getLeft());
+        int rightSize = sizeOfTree(root.getRight());
+        return 1 + leftSize + rightSize;
+    }
+
     // Driver Method AKA Main Method
     public static void main(String[] args){
         BinaryTree tree = new BinaryTree();
@@ -124,6 +263,21 @@ public class BinaryTree {
 //        levelOrderTraversal(tree.root);
 //        System.out.println("Spiral Order Traversal");
 //        spiralOrderTraversalUsingTwoStacks(tree.root);
-        System.out.println("Height of tree : " + heightOfTree(tree.root));
+//        System.out.println("Height of tree : " + heightOfTree(tree.root));
+        BinaryTree tree2 = new BinaryTree();
+        tree2.setRoot(createSampleTree2());
+//        System.out.println("Height of tree2 using Level Order " + heightOfTreeUsingLevelOrder(tree2.getRoot()));
+//        System.out.println("Height of tree2 using recursion " + heightOfTree(tree2.getRoot()));
+//        System.out.println("Level Order traversal line by line of tree2 ");
+//        levelOrderTraversalLineByLine(tree2.getRoot());
+//        System.out.println();
+//        System.out.println("Minimum Depth of tree2 using Level Order Traversal " + minDepthUsingLevelOrderTraversal(tree2.getRoot()));
+//        System.out.println("Minimum Depth of tree using Level Order Traversal " + minDepthUsingLevelOrderTraversal(tree.getRoot()));
+//        System.out.println("Minimum Depth of tree using recursion " + minDepth(tree.getRoot()));
+//        System.out.println("Minimum Depth of tree2 using recursion " + minDepth(tree2.getRoot()));
+        System.out.println("Size of tree " + sizeOfTree(tree.getRoot()));
+        System.out.println("Size of tree2 " + sizeOfTree(tree2.getRoot()));
+
+
     }
 }
